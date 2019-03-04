3c435cad08228fca7fa606cfef95936fc43caf2f
Fonix Hardcastle <actualfriction@gmail.com>
2019-03-04 22:33:02 +0000

    Other

--- MasterThemes/BlackEndSpace/build.sh
+++ MasterThemes/BlackEndSpace/build.sh
@@ -18,7 +18,7 @@ if [ ! -f "artisan" ]; then
     apt install zip -y 2> /dev/null
 
     echo "Backing up previous panel files in the case that something goes wrong!"
-    zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources
+    zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources 2> /dev/null
 
     echo "Downloading the Theme you picked"
     mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
