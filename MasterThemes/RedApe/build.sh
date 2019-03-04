1064f6f89c709611464e2a310786b1926c4b6d3e
Will Hardcastle <actualfriction@gmail.com>
2019-03-04 22:58:20 +0000

    Update build.sh

--- MasterThemes/RedApe/build.sh
+++ MasterThemes/RedApe/build.sh
@@ -2,12 +2,4 @@
 
 if [ ! -f "artisan" ]; then
     echo "File not found"
-    else
-    yum install git zip -y
-    apt install git zip -y
-    zip -r PterodactylBackup-$(date +"%Y-%m-%d").zip public resources
-    mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
-    cp -r MasterThemes/RedApe/public ..
-    cp -r MasterThemes/RedApe/resources ..
-    cd .. && rm -rf tempdown
 fi
