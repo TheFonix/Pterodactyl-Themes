b15a156cdbe1db5a8d0e0f616198f1230392c418
Will Hardcastle <actualfriction@gmail.com>
2019-03-17 15:13:42 +0000

    Update Minecraft Madness Build File

--- MasterThemes/MinecraftMadness/build.sh
+++ MasterThemes/MinecraftMadness/build.sh
@@ -22,8 +22,8 @@ if [ ! -f "artisan" ]; then
 
     echo "Downloading the Theme you picked"
     mkdir -p tempdown && cd tempdown && git clone https://github.com/TheFonix/Pterodactyl-Themes.git .
-    cp -r MasterThemes/BlueBrick/public ..
-    cp -r MasterThemes/BlueBrick/resources ..
+    cp -r MasterThemes/MinecraftMadness/public ..
+    cp -r MasterThemes/MinecraftMadness/resources ..
 
     echo "Files have been copied over!"
     sleep 2
