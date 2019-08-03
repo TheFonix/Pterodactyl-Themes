e74047c678c2ea8278c5ec11a0ed11df12dedde9
Fonix Hardcastle <actualfriction@gmail.com>
2019-08-03 19:02:46 +0100

    New Themes! PinkAnFluffy

---
+++ MasterThemes/PinkAnFluffy/resources/lang/es/pagination.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'next' => 'Siguiente &raquo;',
+    'previous' => '&laquo; Previos ',
+    'sidebar' => [
+        'account_settings' => 'Configuración de la cuenta',
+        'files' => 'Administrador de archivos',
+        'manage' => 'Administrar servidor',
+        'servers' => 'Tus servidores',
+    ],
+];
